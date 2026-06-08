23b8f123e76f319301a02527b8a91c5f256fa6ce
doodleGDnow <doodlegdxd@gmail.com>
2026-06-07 20:49:42 -0500

    Update firebase.js
    added GoogleOAuth

--- js/firebase.js
+++ js/firebase.js
@@ -1,5 +1,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+import { 
+  getAuth, 
+  GoogleAuthProvider 
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
 const firebaseConfig = {
@@ -11,7 +14,7 @@ const firebaseConfig = {
   appId: "1:322265405254:web:cc1ec8a844d788bea1b747"
 };
 
-const app = initializeApp(firebaseConfig);
-
+export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const provider = new GoogleAuthProvider();
