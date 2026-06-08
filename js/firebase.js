e83f4f0d4e4a2df7bb80300e1995923454fb1b4e
doodleGDnow <doodlegdxd@gmail.com>
2026-06-07 20:40:11 -0500

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAM93Sb-qBDOQ7pWi0OTuebjLBU8k2Hjkk",
+  authDomain: "gdcommunity-825cb.firebaseapp.com",
+  projectId: "gdcommunity-825cb",
+  storageBucket: "gdcommunity-825cb.appspot.com",
+  messagingSenderId: "322265405254",
+  appId: "1:322265405254:web:cc1ec8a844d788bea1b747"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
