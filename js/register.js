af8c057f9b13aee8b870986c21bb1bad6974869b
doodleGDnow <doodlegdxd@gmail.com>
2026-06-07 21:31:45 -0500

    Update register.js

--- js/register.js
+++ js/register.js
@@ -1,20 +1,27 @@
+import { auth } from "./firebase.js";
 import { createUserWithEmailAndPassword } from "firebase/auth";
+import { db } from "./firebase.js";
 import { doc, setDoc, serverTimestamp } from "firebase/firestore";
-import { auth, db } from "./firebase.js";
 
-window.register = async function () {
+document.getElementById("registerBtn").addEventListener("click", register);
+
+async function register() {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
   const username = document.getElementById("username").value;
 
-  const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-  const user = userCredential.user;
+  try {
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    const user = userCredential.user;
 
-  await setDoc(doc(db, "users", user.uid), {
-    username: username,
-    email: email,
-    createdAt: serverTimestamp()
-  });
+    await setDoc(doc(db, "users", user.uid), {
+      username: username,
+      email: email,
+      createdAt: serverTimestamp()
+    });
 
-  alert("Usuario creado 🔥");
-};
+    console.log("Usuario creado 🔥");
+  } catch (error) {
+    console.log(error.message);
+  }
+}
